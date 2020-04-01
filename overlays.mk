8b03d47a311963b95c896fa7536076da13d1be00
Genkzsz11 <Genkzsz11@gmail.com>
2020-04-01 20:03:15 +0700

    Revert "Revert "optional miui-like brightness slider [2/3]""
    This reverts commit 1e8e13cba0bcd482a39d316bc1df0837c5191e1e.

--- overlays.mk
+++ overlays.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# MiuiSlider
+PRODUCT_PACKAGES += \
+    BrightnessSliderOverlay
+
 # Icon shape overlays
 PRODUCT_PACKAGES += \
     IconShapeRoundedRectOverlay \
