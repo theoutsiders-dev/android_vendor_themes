9caf1cac6c95ac6229f260fb662699fe1cc0d632
Genkzsz11 <Genkzsz11@gmail.com>
2020-04-21 02:21:36 +0700

    bootanimation: update

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,7 +1,15 @@
 # Boot Animation
 
-scr_resolution := 1080
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
+
+
+ifneq ($(filter ysl land,$(du_device)),)
+scr_resolution := 720
+endif
+
+ifneq ($(filter lavender mido raphael wayne whyred platina gingko vince tulip X00T enchilada,$(du_device)),)
+scr_resolution := 1080
+endif
 
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
